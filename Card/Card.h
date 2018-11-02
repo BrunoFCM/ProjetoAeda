3175cdb76e93299c7b0654bd7cb307accfdc5df6
Miguel Pinto <43591348+MiguelDelPinto@users.noreply.github.com>
2018-11-02 16:04:19 +0000

    Changed commentaries

--- Card/Card.h
+++ Card/Card.h
@@ -6,50 +6,55 @@ class Card{
 	int balance; //correspondente ao saldo do cartão
 	string card_number; //correspondente ao número de identificação do cartão
 	bool valid;	//indica a validade do número do cartão
-	bool check_validity();
+
 	/**
 	 * @brief Função que utiliza o algoritmo de Luhn para verificar a validade do número do cartão
 	 * @return True se o valor de card_number for válido, caso contrário retorna false
 	 */
+	bool check_validity();
+	
 public:
-	Card(string number);
 	/**
 	 * @brief Construtor da classe Card usando apenas o número do cartão
 	 * @param number - número do cartão
 	 */
-	Card(string number, int bal);
+	Card(string number);
+	
 	/**
 	 * @brief Construtor da classe Card usando o número do cartão e o respetivo saldo
 	 * @param number - número do cartão
 	 * @param bal - valor do saldo do cartão
 	 */
-	bool set_balance(int bal);
+	Card(string number, int bal);
+	
 	/**
 	 * @brief Função que altera o valor de balance, caso o número do cartão seja válido
 	 * @param bal - valor do saldo do cartão
 	 * @return True se o valor de balance for alterado com sucesso
 	 */
-	void set_number(string number);
+	bool set_balance(int bal);
+	
 	/**
 	 * @brief Função que altera o número do cartão, e de seguida o valor de valid a partir da função check_validity()
 	 * @param number - número do cartão
 	 */
-	int get_balance() const;
+	void set_number(string number);
+	
 	/**
 	 * @brief Função que retorna o valor do saldo do cartão (balance)
 	 * @return balance - saldo do cartão
 	 */
-	bool get_validity() const;
+	int get_balance() const;
+	
 	/**
 	 * @brief Função que retorna se o número do cartão é válido (valid)
 	 * @return valid - True se o número do cartão é válido
 	 */
-	string get_number() const;
+	bool get_validity() const;
+	
 	/**
 	 * @brief Função que retorna o número do cartão
 	 * @return card_number - número do cartão
 	 */
+	string get_number() const;
 };
-
-
-
