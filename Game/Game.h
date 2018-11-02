f2e0b1dc209a2ed8337509897c77299d339357f3
nmtc01 <39002030+nmtc01@users.noreply.github.com>
2018-11-02 12:51:02 +0000

    Update Game.h
    Added declarations of function members to class Game.

--- Game/Game.h
+++ Game/Game.h
@@ -13,16 +13,83 @@ using namespace std;
 class Game
 {
 private:
-	int id;							//identificador único
-	string title;					//nome
-	int price;						//preço de aquisição
-	Date release;					//data de lançamento
-	Interval age_range;				//intervalo de idades
-	vector<string> platform;		//plataformas disponíveis
-	char genre;						//género
-	string developer;				//empresa que o desenvolveu
-	vector<int> price_history;		//historial de preços de aquisição
+	unsigned int id;						//identificador Ãºnico
+	string title;							//nome
+	unsigned int price;						//preco de aquisicao
+	Date release;							//data de lancamento
+	Interval age_range;						//intervalo de idades
+	vector<string> platform;				//plataformas disponiveis
+	char genre;								//genero
+	string developer;						//empresa que o desenvolveu
+	vector<unsigned int> price_history;		//historial de precos de aquisicao
 public:
+
+	/**
+	* @brief Construtor da classe Game
+	* @param title - nome do jogo
+	* @param price - preco de aquisicao do jogo
+	* @param release - data de lancamento do jogo
+	* @param age_range - faixa etaria a que se tem de pertencer para se poder jogar 
+	* @param platform - vetor de plataformas para as quais o jogo se encontra disponivel
+	* @param genre - genero (F ou M) ao qual o jogo se destina (preferencialmente)
+	* @param developer - empresa respnsavel pela criacao do jogo
+	* @param price_history - historial de precos de aquisicao do jogo
+	*/
+	Game(string title, unsigned int price, Date release, Interval age_range, vector<string> platform, char genre, string developer, vector<unsigned int> price_history);
+
+	/**
+	* @brief Membro-funcao que retorna o identificador Ãºnico de um jogo
+	* @return Retorna o identificador Ãºnico de um jogo
+	*/
+	unsigned int getId() const;
+
+	/**
+	* @brief Membro-funcao que retorna o titulo de um jogo
+	* @return Retorna o titulo de um jogo
+	*/
+	string getTitle() const;
+
+	/**
+	* @brief Membro-funcao que retorna o preco de aquisicao de um jogo
+	* @return Retorna o preco de aquisicao de um jogo
+	*/
+	unsigned int getPrice() const;
+
+	/**
+	* @brief Membro-funcao que retorna a data de lancamento de um jogo
+	* @return Retorna a data de lancamento de um jogo
+	*/
+	Data getRelease() const;
+
+	/**
+	* @brief Membro-funcao que retorna a faixa etaria a que se tem de pertencer para se poder jogar
+	* @return Retorna um intervalo de idades
+	*/
+	Interval getAge() const;
+
+	/**
+	* @brief Membro-funcao que retorna um vetor de plataformas para as quais o jogo se encontra disponivel
+	* @return Retorna um vetor de plataformas
+	*/
+	vector<string> getPlatforms() const;
+
+	/**
+	* @brief Membro-funcao que retorna um caracter correspondente ao genero a que o jogo se destina
+	* @return Retorna um caracter corrspondente ao genero
+	*/
+	char getGenre() const;
+
+	/**
+	* @brief Membro-funcao que retorna uma string correspondente ao nome da empresa que desenvolveu o jogo
+	* @return Retorna o nome da empresa
+	*/
+	string getDeveloper() const;
+
+	/**
+	* @brief Membro-funcao que retorna o historial de precos de aquisicao do jogo
+	* @return Retorna o historial de precos de aquisicao do jogo
+	*/
+	vector<unsigned int> getPriceHist() const;
 
 };
 
@@ -39,7 +106,7 @@ public:
 class Fixed_Subsc : public Online
 {
 private:
-	int fixed_price;				 //preço fixo
+	int fixed_price;				 //preÃ§o fixo
 public:
 
 };
@@ -47,7 +114,7 @@ public:
 class Variable_Subsc : public Online
 {
 private:
-	int price_hour;					 //preço variável: custo do jogo por hora
+	int price_hour;					 //preÃ§o variÃ¡vel: custo do jogo por hora
 public:
 
 };
@@ -55,7 +122,7 @@ public:
 class Home : public Game
 {
 private:
-	Date updates;					 //data das atualizações em que o utilizador fez download do respetivo título (1€ cada)
+	Date updates;					 //data das atualizaÃ§Ãµes em que o utilizador fez download do respetivo tÃ­tulo (1â‚¬ cada)
 public:
 
 };
