22c4d8de26df8d35a0d17f5b7e17f84bcce2d464
nmtc01 <39002030+nmtc01@users.noreply.github.com>
2018-11-03 15:43:07 +0000

    Changed type of balance to double

--- Card/Card.h
+++ Card/Card.h
@@ -5,57 +5,59 @@
 using namespace std;
 
 class Card{
-	int balance; //correspondente ao saldo do cartão
-	string card_number; //correspondente ao número de identificação do cartão
-	bool valid;	//indica a validade do número do cartão
+	double balance; //correspondente ao saldo do cartao
+	string card_number; //correspondente ao numero de identificao do cartao
+	bool valid;	//indica a validade do numero do cartao
 
 	/**
-	 * @brief Função que utiliza o algoritmo de Luhn para verificar a validade do número do cartão
+	 * @brief Funcao que utiliza o algoritmo de Luhn para verificar a validade do numero do cartao
 	 */
-	void checkValidity();
-	
+
 public:
+	
 	/**
-	 * @brief Construtor da classe Card usando apenas o número do cartão
-	 * @param number - número do cartão
+	 * @brief Construtor da classe Card usando apenas o numero do cartao
+	 * @param number - numero do cartao
 	 */
 	Card(string number);
 	
 	/**
-	 * @brief Construtor da classe Card usando o número do cartão e o respetivo saldo
-	 * @param number - número do cartão
-	 * @param bal - valor do saldo do cartão
+	 * @brief Construtor da classe Card usando o numero do cartao e o respetivo saldo
+	 * @param number - numero do cartao
+	 * @param bal - valor do saldo do cartao
 	 */
-	Card(string number, int bal);
+	Card(string number, double bal);
 	
+	void checkValidity();
+
 	/**
-	 * @brief Função que altera o valor de balance, caso o número do cartão seja válido
-	 * @param bal - valor do saldo do cartão
+	 * @brief Funcao que altera o valor de balance, caso o numero do cartao seja valido
+	 * @param bal - valor do saldo do cartao
 	 * @return True se o valor de balance for alterado com sucesso
 	 */
-	bool setBalance(int bal);
+	bool setBalance(double bal);
 	
 	/**
-	 * @brief Função que altera o número do cartão, e de seguida o valor de valid a partir da função check_validity()
-	 * @param number - número do cartão
+	 * @brief Funcao que altera o numero do cartao, e de seguida o valor de valid a partir da funcao check_validity()
+	 * @param number - numero do cartao
 	 */
 	void setNumber(string number);
 	
 	/**
-	 * @brief Função que retorna o valor do saldo do cartão (balance)
-	 * @return balance - saldo do cartão
+	 * @brief Funcao que retorna o valor do saldo do cartao (balance)
+	 * @return balance - saldo do cartao
 	 */
-	int getBalance() const;
+	double getBalance() const;
 	
 	/**
-	 * @brief Função que retorna se o número do cartão é válido (valid)
-	 * @return valid - True se o número do cartão é válido
+	 * @brief Funcao que retorna true se o numero do cartao for valido (valid)
+	 * @return valid - True se o numero do cartao for valido
 	 */
 	bool getValidity() const;
 	
 	/**
-	 * @brief Função que retorna o número do cartão
-	 * @return card_number - número do cartão
+	 * @brief Funcao que retorna o numero do cartao
+	 * @return card_number - numero do cartao
 	 */
 	string getNumber() const;
 };
