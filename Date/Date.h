242d92b2c9ce364bf87391273b836638343d476b
nmtc01 <39002030+nmtc01@users.noreply.github.com>
2018-11-03 15:28:48 +0000

    Update Date.h

--- Date/Date.h
+++ Date/Date.h
@@ -3,12 +3,12 @@
 #include <string>
 
 /**
- *  Classe Date com várias implementações: data de lançamento de um jogo, data em que um utilizador jogou um determinado jogo, etc. (ALTERAR DEPOIS)
+ *  Classe Date com varias implementacoes: data de lancamento de um jogo, data em que um utilizador jogou um determinado jogo, etc. (ALTERAR DEPOIS)
  */
 class Date {
 private:
 	unsigned int day;			//Dia referente a uma data
-	unsigned int month;			//Mês referente a uma data
+	unsigned int month;			//Mes referente a uma data
 	unsigned int year;			//Ano referente a uma data
 
 public:
@@ -19,35 +19,36 @@ public:
 	Date(const std::string &date);
 
 	/**
-	 * @brief Construtor da classe Date usando números inteiros
+	 * @brief Construtor da classe Date usando numeros inteiros
 	 * @param d - dia de uma data
-	 * @param m - mês de uma data
+	 * @param m - mes de uma data
 	 * @param y - ano de uma data
 	 */
 	Date(unsigned int d, unsigned int m, unsigned int y);
 
 	/**
-	 * @brief Membro função que retorna o dia de uma data
+	 * @brief Membro funcao que retorna o dia de uma data
 	 * @return Retorna o dia de uma data
 	 */
 	unsigned int getDay() const;
 
 	/**
-	 * @brief Membro função que retorna o mês de uma data
-	 * @return Retorna o mês de uma data
+	 * @brief Membro funcao que retorna o mes de uma data
+	 * @return Retorna o mes de uma data
 	 */
 	unsigned int getMonth() const;
 
 	/**
-	 * @brief Membro função que retorna o ano de uma data
+	 * @brief Membro funcao que retorna o ano de uma data
 	 * @return Retorna o ano de uma data
 	 */
 	unsigned int getYear() const;
 
 	/**
-	 * @brief Membro função que permite obter uma data no formato DD-MM-AAAA
+	 * @brief Membro funcao que permite obter uma data no formato DD-MM-AAAA
 	 * @return Retorna string de uma data no formato DD-MM-AAAA
 	 */
 	std::string getDate() const;
 
 #endif /* DATE_H_ */
+
