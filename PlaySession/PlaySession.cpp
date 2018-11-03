0d2d0a96f4e8c489d567a4c37b5071231d0bda8c
nmtc01 <39002030+nmtc01@users.noreply.github.com>
2018-11-03 12:34:34 +0000

    Implemented function members

--- PlaySession/PlaySession.cpp
+++ PlaySession/PlaySession.cpp
@@ -3,6 +3,34 @@
 
 using namespace std;
 
+PlaySession::PlaySession(Date date, int duration, string platform, User* user)
+{
+	this->date = date;
+	this->duration = duration;
+	this->user = user;
+}
+
+Date PlaySession::getDate() const
+{
+	return date;
+}
+
+unsigned int PlaySession::getDuration() const
+{
+	return duration;
+}
+
+string PlaySession::getPlatform() const
+{
+	return platform;
+}
+
+User* PlaySession::getUser() const
+{
+	return user;
+}
+
+
 
 
 
