bc34bff098d69953f288fcca513b9fde541789da
Miguel Pinto <43591348+MiguelDelPinto@users.noreply.github.com>
2018-11-03 19:19:13 +0000

    Fixed exception handling

--- System/System.cpp
+++ System/System.cpp
@@ -6,19 +6,26 @@ using namespace std;
 
 void System::addUser(User* user)
 {
-	for (size_t i = 0; i < user_library.size(); i++)
-		if (user == user_library[i])
-			throw RepeatedUser();
+	for (size_t i = 0; i < user_library.size(); i++) {
+		if (user == user_library[i]) {
+			string info = user->getName();
+			throw RepeatedUser(info);
+		}
+	}
 	user_library.push_back(user);
 }
 
 void System::addGame(Game* game)
 {
-	for (size_t i = 0; i < store.size(); i++)
-		if (game == store[i])
-			throw RepeatedGame();
+	for (size_t i = 0; i < store.size(); i++) {
+		if (game == store[i]) {
+			string info = to_string(game->getId());
+			throw RepeatedGame(info);
+		}
+	}
 	store.push_back(game);
 }
+
 
 
 
