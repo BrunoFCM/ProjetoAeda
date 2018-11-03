f297299d16edd88c9757bf5f4f94edfab4293cc9
nmtc01 <39002030+nmtc01@users.noreply.github.com>
2018-11-03 15:18:42 +0000

    Changed function types to void

--- System/System.cpp
+++ System/System.cpp
@@ -3,29 +3,22 @@
 
 using namespace std;
 
-bool System::addUser(User* user)
+void System::addUser(User* user)
 {
 	for (size_t i = 0; i < user_library.size(); i++)
 		if (user == user_library[i])
-		{
 			throw RepeatedUser();
-			return false;
-		}
 	user_library.push_back(user);
-	return true;
 }
 
-bool System::addGame(Game* game)
+void System::addGame(Game* game)
 {
 	for (size_t i = 0; i < store.size(); i++)
 		if (game == store[i])
-		{
 			throw RepeatedGame();
-			return false;
-		}
 	store.push_back(game);
-	return true;
 }
+
 
 
 
