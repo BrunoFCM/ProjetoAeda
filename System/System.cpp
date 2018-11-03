2c569de6385ca91df29fb4c9ea0d5f9a2534ac32
nmtc01 <39002030+nmtc01@users.noreply.github.com>
2018-11-03 13:31:43 +0000

    Implemented member functions

--- System/System.cpp
+++ System/System.cpp
@@ -1,10 +1,31 @@
-/*
- * System.cpp
- *
- *  Created on: 03/11/2018
- *      Author: Nuno Cardoso
- */
+#include <iostream>
+#include "System.h"
 
+using namespace std;
+
+bool System::addUser(User* user)
+{
+	for (size_t i = 0; i < user_library.size(); i++)
+		if (user == user_library[i])
+		{
+			throw RepeatedUser();
+			return false;
+		}
+	user_library.push_back(user);
+	return true;
+}
+
+bool System::addGame(Game* game)
+{
+	for (size_t i = 0; i < store.size(); i++)
+		if (game == store[i])
+		{
+			throw RepeatedGame();
+			return false;
+		}
+	store.push_back(game);
+	return true;
+}
 
 
 
