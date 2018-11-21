ddb9a9186f4d39e205d14c97a2cb9619788b6293
Miguel Pinto <43591348+MiguelDelPinto@users.noreply.github.com>
2018-11-21 19:21:50 +0000

    Added some features and Doxygen Documentation

--- PlaySession/PlaySession.h
+++ PlaySession/PlaySession.h
@@ -10,18 +10,48 @@ using namespace std;
 class Date;
 class User;
 
+/**
+ * Classe PlaySession que contém os dados sobre cada sessão de jogo realizada
+ */
 class PlaySession
 {
 private:
-	Date date;
-	unsigned int duration;
-	string platform;
-	User* user;
+	Date date;					//
+	unsigned int duration;		//
+	string platform;			//
+	User* user;					//
 public:
+	/**
+	 *@brief - Construtor da classe PlaySession
+	 *@param date -
+	 *@param duration -
+	 *@param platform -
+	 *@param user -
+	 */
 	PlaySession(const Date date, const int duration, const string platform, User* user);
+
+	/**
+	 * @brief
+	 * @return
+	 */
 	Date getDate() const;
+
+	/**
+	 * @brief
+	 * @return
+	 */
 	unsigned int getDuration() const;
+
+	/**
+	 * @brief
+	 * @return
+	 */
 	string getPlatform() const;
+
+	/**
+	 * @brief
+	 * @return
+	 */
 	User* getUser() const;
 };
 
