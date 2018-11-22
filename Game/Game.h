a5a8182ed052c86fd6452a41a2a3c4f381638b9c
Miguel Pinto <43591348+MiguelDelPinto@users.noreply.github.com>
2018-11-22 23:19:44 +0000

    Added member functions and Doxy documentation

--- Game/Game.h
+++ Game/Game.h
@@ -1,5 +1,7 @@
-#pragma once
+#ifndef SRC_GAME_H_
+#define SRC_GAME_H_
 
+#pragma once
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,8 +12,9 @@
 
 using namespace std;
 
+class Date;
 class User;
-
+class Interval;
 
 class Game
 {
@@ -27,19 +30,25 @@ private:
 	string developer;						//empresa que o desenvolveu
 	vector<double> price_history;			//historial de precos de aquisicao
 	vector<User*> users;				    //vetor de utilizadores
+	static unsigned int id_seq;
 public:
 
 	/**
-	* @brief Construtor da classe Game
-	* @param title - nome do jogo
-	* @param price - preco de aquisicao do jogo
-	* @param release - data de lancamento do jogo
-	* @param age_range - faixa etaria a que se tem de pertencer para se poder jogar 
-	* @param platform - vetor de plataformas para as quais o jogo se encontra disponivel
-	* @param genre - vetor de generos dos jogos
-	* @param developer - empresa respnsavel pela criacao do jogo
-	*/
-	Game(const string &title, const double &price, const Date &release, const Interval &age_range, const vector<string> &platforms, const vector<string> &genres, const string &developer);
+	 * @brief Construtor da classe Game
+	 * @param title Nome do jogo
+	 * @param price Preco de aquisicao do jogo
+	 * @param release Data de lancamento do jogo
+	 * @param age_range Faixa etaria a que se tem de pertencer para se poder jogar
+	 * @param platforms Vetor de plataformas para as quais o jogo se encontra disponivel
+	 * @param genres Vetor de generos dos jogos
+	 * @param developer Empresa responsavel pela criacao do jogo
+	 */
+	Game(const string &title,const double &price,const Date &release,const Interval &age_range,const vector<string> &platforms,const vector<string> &genres,const string &developer);
+
+	/**
+	 * @brief Destrutor da classe Game
+	 */
+	~Game();
 
 	/**
 	* @brief Membro-funcao que retorna o identificador unico de um jogo
@@ -103,64 +112,209 @@ public:
 
 	/**
 	* @brief Overload do operador == para jogos
+	* @param game Jogo com qual e comparado
 	* @return Retorna true se o id dos dois jogos for igual e false caso contrario
 	*/
 	bool operator==(Game &game);
 
+	/**
+	* @brief Altera o preço atual do jogo
+	* @param newPrice Novo preco do jogo
+	*/
 	void changePrice(const double &newPrice);
 
+	/**
+	* @brief Define um desconto para um jogo
+	* @param percentage Desconto a definir
+	*/
 	void discountPrice(const unsigned int &percentage);
 
+	/**
+	* @brief Altera o preço base do jogo
+	* @param newPrice Novo preco base do jogo
+	*/
 	void changeBasePrice(const double &newPrice);
 
-	void revertToPrice(const unsigned int &number);
+	/**
+	* @brief Altera o preço para o preco base
+	*/
+	void revertToPrice();
 
+	/**
+	* @brief Adiciona um utilizador a base de jogadores de jogo
+	* @param user Utilizador a adicionar
+	*/
 	void addUser(User* user);
+
+
+	/**
+	 * @brief Membro puramente virutal que permite obter informacao sobre um determinado jogo. Encontra-se implementado nas classes derivadas Home e Online
+	 */
+	virtual void printInfoGame() const = 0;
 };
+
 
 
 class PlaySession;
 
+/**
+ * Classe Online derivada da classe Game
+ */
 class Online : public Game
 {
 private:
 	int play_time;					//tempo jogado
-	vector <PlaySession *> play_history;		//historial para cada jogo online que inclui: quando foi jogado, por quanto tempo e em que plataforma
+	PlaySession play_history;		//historial para cada jogo online que inclui: quando foi jogado, por quanto tempo e em que plataforma
 public:
+	/**
+	 * @brief Construtor da classe Online
+	 * @param title Nome do jogo
+	 * @param price Preco de aquisicao do jogo
+	 * @param release Data de lancamento do jogo
+	 * @param age_range Faixa etaria a que se tem de pertencer para se poder jogar
+	 * @param platforms Vetor de plataformas para as quais o jogo se encontra disponivel
+	 * @param genres Vetor de generos dos jogos
+	 * @param developer Empresa responsavel pela criacao do jogo
+	 */
 	Online(const string &title, const double &price, const Date &release, const Interval &age_range, const vector<string> &platforms, const vector<string> &genres, const string &developer);
+
+	/**
+	 *
+	 */
 	virtual ~Online() {};
+
+	/**
+	 * @brief Devolve o tempo total jogado
+	 */
 	int getPlayTime() const;
-	vector<PlaySession*> getPlayHistory() const;
+
+	/**
+	 * @brief Devolve o valor 0
+	 */
 	virtual double getPrice() const;
+
+	/**
+	 * @brief Permite aceder ao conjunto de sessoes efetuadas no jogo
+	 * @return Vetor apontador para o conjunto de objetos de classe PlaySession
+	 */
+	vector<PlaySession*> getPlayHistory() const;
 };
 
+
+
+/**
+ * Classe FixedSubsc derivada da classe Online
+ */
 class FixedSubsc : public Online
 {
 private:
-	double fixed_price;				 //preco fixo
+	int fixed_price;				 //preco fixo
 public:
+
+	/**
+	 * @brief Construtor da classe FixedSubsc
+	 * @param title Nome do jogo
+	 * @param price Preco de aquisicao do jogo
+	 * @param release Data de lancamento do jogo
+	 * @param age_range Faixa etaria a que se tem de pertencer para se poder jogar
+	 * @param platforms Vetor de plataformas para as quais o jogo se encontra disponivel
+	 * @param genres Vetor de generos dos jogos
+	 * @param developer Empresa responsavel pela criacao do jogo
+	 * @param fixed_price Preco de subscricao fixa
+	 */
 	FixedSubsc(const string &title, const double &price, const Date &release, const Interval &age_range, const vector<string> &platforms, const vector<string> &genres, const string &developer, const double &fixed_price);
+
+	/**
+	 * @brief Devolve o preco da subscricao fixa
+	 * @return Preco da subscricao fixe
+	 */
 	double getPrice() const;
+
+	/**
+	 * @brief Imprime informacao relativamente ao jogo
+	 */
+	void printInfoGame() const;
+
 };
 
+
+
+/**
+ * Classe VariableSubsc derivada da classe Online
+ */
 class VariableSubsc : public Online
 {
 private:
 	int price_hour;					 //preco variavel: custo do jogo por hora
 public:
+
+	/**
+	 * @brief Construtor da classe VariableSubsc
+	 * @param title Nome do jogo
+	 * @param price Preco de aquisicao do jogo
+	 * @param release Data de lancamento do jogo
+	 * @param age_range Faixa etaria a que se tem de pertencer para se poder jogar
+	 * @param platforms Vetor de plataformas para as quais o jogo se encontra disponivel
+	 * @param genres Vetor de generos dos jogos
+	 * @param developer Empresa responsavel pela criacao do jogo
+	 * @param price_hour Preco pago por cada hora jogada
+	 */
 	VariableSubsc(const string &title, const double &price, const Date &release, const Interval &age_range, const vector<string> &platforms, const vector<string> &genres, const string &developer, const double &price_hour);
+
+	/**
+	 * @brief Devolve o preco pago atual, dependente do preco por hora e pelo tempo jogado
+	 * @return Preco pago atual
+	 */
 	double getPrice() const;
+
+	/**
+	 * @brief Imprime informacao relativamente ao jogo
+	 */
+	void printInfoGame() const;
+
 };
 
+
+
+
+
+/**
+ * Classe Home derivada da classe Game
+ */
 class Home : public Game
 {
 private:
 	vector<Date> updates;					 //data das atualizacoes em que o utilizador fez download do respetivo titulo (1 euro cada)
 public:
+	/**
+	 * @brief Construtor da classe Home
+	 * @param title Nome do jogo
+	 * @param price Preco de aquisicao do jogo
+	 * @param release Data de lancamento do jogo
+	 * @param age_range Faixa etaria a que se tem de pertencer para se poder jogar
+	 * @param platforms Vetor de plataformas para as quais o jogo se encontra disponivel
+	 * @param genres Vetor de generos dos jogos
+	 * @param developer Empresa responsavel pela criacao do jogo
+	 */
 	Home(const string &title, const double &price, const Date &release, const Interval &age_range, const vector<string> &platforms, const vector<string> &genres, const string &developer);
+
+	/**
+	 * @brief Devolve um vetor com todos os updates feitos ao jogo
+	 * @return Vetor de datas de update
+	 */
 	vector<Date> getUpdates() const;
+
+	/**
+	 * @brief Acrescenta um update ao vetor de update
+	 * @param date Data do update a acrescentar
+	 */
+	void addUpdate(Date date) const;
+
+	/**
+	 * @brief Imprime informacao relativamente ao jogo
+	 */
+	void printInfoGame() const;
 };
 
 
-
-
+#endif /* SRC_GAME_H_ */
